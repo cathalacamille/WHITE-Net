c6b8ff15b0a5b39ad476e47db05a57164ca8f70d
Camille Cathala <camillecathala17@orange.fr>
2026-04-29 15:29:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open('requirements.txt', 'rt') as f:
     required_packages = [l.strip() for l in f.readlines()]
 
 setup(name='WHITENet',
-	version='1.0.0',
+	version='2.0.0',
 	description='White matter HyperIntensities Tissue Extraction using deep-learning Network',
 	author='Camille Cathala',
-    author_email='camille.cathala@epfl.ch',
+    author_email='camille.cathala@epfl.alumni.ch',
     url='https://github.com/cathalacamille/WHITE-Net',
 	install_requires=required_packages,
     entry_points={
@@ -17,7 +17,7 @@ setup(name='WHITENet',
         ],
     },
     package_data={
-        'WHITENet': ['white_net_FLAIR.pt','white_net_FLAIR_T1w.pt'],  # Include the file in the package
+        'WHITENet': ['white_net_FLAIR.pt'],  # Include the file in the package
     },
 	packages=find_packages(),
 	include_package_data=True)
